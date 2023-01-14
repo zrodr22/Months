032f94ba91d5edcfc89146ee4a08343ee1b00b7e
zrodr22 <zrodr24@gmail.com>
2023-01-14 14:26:58 -0800

    Added an install folder

--- install/months_install.bat
+++ install/months_install.bat
@@ -1,4 +1,4 @@
 @REM pyinstaller months_setup.py
-pyinstaller months_setup.py --icon=tree.ico
+pyinstaller --windowed ../src/months_setup.py --icon=../images/tree.ico
 @REM cd ../../../Desktop
 @REM mklink /d Months c:\Users\zrodr\Documents\Projects\Months\dist\Month_Reflections\Month_Reflections
